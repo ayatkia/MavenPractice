e112781cdc71228700d5121d1aba647a1a1c6852
Ayat <a.kyanejad@yahoo.com>
2026-08-29 22:07:07 +0330

    Revert "Add version 3 message"
    This reverts commit 44f802df689d96ea329eee9b76bacc9d4bbae142.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Version 3");
+        System.out.println("Hello Maven + Git");
     }
 }
