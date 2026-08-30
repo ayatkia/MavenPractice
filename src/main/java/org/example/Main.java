c4fb916b4ba7000d1000ef4413bd6c61c3705da5
Ayat <a.kyanejad@yahoo.com>
2026-08-30 21:05:20 +0330

    Update main on discount branch

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello Maven + Git");
+        System.out.println("Hello Maven + Git - Discount");
     }
 }
